15cace8ce2f37240f2cd880b1b6caee588ad5b21
lauramoraesg2 <laurinha.anjinha@gmail.com>
2012-12-22 14:12:43 -0200

    criação do projeto

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MadMonkey::Application.config.secret_token = 'eae5417cfa694671240f2ec852960c817d320dc0ba3cc1c6437b4d797768916eef8f6f9eac85d63d2f439930396eb3c86070bf66dec9efa3032e07260bd75a44'
